911b3c317f6c3ffc29de4b3226609bbab80b60c7
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-22 11:22:38 +0100

    add iseven

---
+++ IsEven/IsEven.java
@@ -0,0 +1,9 @@
+public class IsEven {
+    public static boolean isEven(int a) {
+        // your code here
+        if (a % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+}
