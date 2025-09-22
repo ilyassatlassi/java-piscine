ef03129deb27b8a4db5c809deb126bced56967c1
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-22 12:15:01 +0100

    add stringConcat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,7 +1,11 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
         // your code here
-       
-        return s1.concat(s2);
+        if (s1 != null && s2 != null) {
+            return s1.concat(s2);
+        } else if (s1 == null) {
+            return s2;
+        }
+        return s1;
     }
 }
