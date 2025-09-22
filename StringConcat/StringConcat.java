264ce270828b9fddfb552f11782b00bce7a4d2e9
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-22 12:11:51 +0100

    add stringConcat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,6 +1,7 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
         // your code here
-        return s1 + s2;
+       
+        return s1.concat(s2);
     }
 }
