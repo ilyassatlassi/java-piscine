463b3ff58a63519996a7832cfa99604dd61650bd
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-22 17:12:11 +0100

    add computeArray

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,16 @@
+public class ComputeArray {
+    public static int[] computeArray(int[] array) {
+        // your code here
+        int[] res = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                res[i] = array[i] * 5;
+            } else if ((array[i] - 1) % 3 == 0) {
+                res[i] = array[i] + 7;
+            } else /*if ((array[i] - 2) % 3 == 0)*/ {
+                res[i] = array[i] ;
+            }
+        }
+        return res;
+    }
+}
