0b1563d096f7a1a051d8a43dd387b59b7954563a
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-22 11:18:33 +0100

    add floatOperation

---
+++ student/IntOperations/IntOperations.java
@@ -0,0 +1,21 @@
+public class IntOperations {
+    public static int addTwoIntegers(int a, int b) {
+        // your code here
+        return a + b;
+    }
+
+    public static int subtractTwoIntegers(int a, int b) {
+        // your code here
+        return a - b;
+    }
+
+    public static int multiplyTwoIntegers(int a, int b) {
+        // your code here
+        return a * b;
+    }
+
+    public static int divideTwoIntegers(int a, int b) {
+        // your code here
+        return a / b;
+    }
+}
