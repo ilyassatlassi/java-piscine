1e211dd07f4788408ddfdf27db81326829f89327
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-22 12:51:59 +0100

    add stringReplace

--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -1,9 +1,17 @@
 public class StringReplace {
-    public static String replace(String s, CharSequence target, CharSequence replacement) {
+    public static String replace(String s, String target, String replacement) {
         // your code here
         if (s == null || target == null || replacement == null) {
-           return ""; 
+            return "";
         }
-        return s.replace(target, replacement); 
+        return s.replace(target, replacement);
+    }
+
+    public static String replace(String s, char target, char replacement) {
+        // your code here
+        if (s == null ) {
+            return "";
+        }
+        return s.replace(target, replacement);
     }
 }
