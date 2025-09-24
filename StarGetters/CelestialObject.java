32cc364852482d694aea13866a81dd5850a1a366
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-24 12:59:39 +0100

    StarGetters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,50 @@
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public CelestialObject() {
+        this("Soleil", 0, 0, 0);
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+}
