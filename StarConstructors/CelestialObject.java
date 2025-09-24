8cf2ad138efeea4dcf27a0b877507c56e6ac1f7b
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-24 11:46:24 +0100

    StarConstructors

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,18 @@
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public CelestialObject() {
+        this("Soleil", 0, 0, 0);
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+
+    }
+}
