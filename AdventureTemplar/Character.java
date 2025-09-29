02d9743f871171a5d3d6d7ae86bdaffa86b4dc98
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-29 17:44:47 +0100

    AdventureTemplar

---
+++ AdventureTemplar/Character.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Character {
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+    private static List<Character> allCharacters = new ArrayList<>();
+
+    public Character(String name, int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.name = name;
+        this.currentHealth = maxHealth;
+        allCharacters.add(this);
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(int in) {
+        if (in + this.currentHealth > maxHealth) {
+
+            this.currentHealth += (in + this.currentHealth - maxHealth);
+            return;
+        }
+        this.currentHealth += in ;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        if (getCurrentHealth() == 0) {
+            return getName() + " : KO";
+        }
+        return getName() + " : " + getCurrentHealth() + "/" + getMaxHealth();
+    }
+
+    public void takeDamage(int par) {
+        currentHealth -= par;
+        if (currentHealth < 0) {
+            currentHealth = 0;
+        }
+    }
+
+    public void attack(Character par) {
+        par.takeDamage(9);
+    }
+
+    public static String printStatus() {
+        if (allCharacters.isEmpty()) {
+            return "------------------------------------------\n" +
+                    "Nobody's fighting right now !\n" +
+                    "------------------------------------------\n";
+
+        }
+        StringBuilder res = new StringBuilder();
+        res.append("------------------------------------------\n");
+
+        res.append("Characters currently fighting :\n");
+
+        for (Character character : allCharacters) {
+            res.append(" - " + character.toString() + "\n");
+        }
+
+        res.append("------------------------------------------\n");
+        return res.toString();
+    }
+
+    public static Character fight(Character obj1, Character obj2) {
+        while (obj1.currentHealth != 0 && obj2.currentHealth != 0) {
+            obj1.attack(obj2);
+            if (obj2.currentHealth == 0)
+                break;
+            obj2.attack(obj1);
+        }
+        if (obj1.currentHealth == 0) {
+            return obj2;
+        }
+        return obj1;
+    }
+}
---
+++ AdventureTemplar/Sorcerer.java
@@ -0,0 +1,27 @@
+public class Sorcerer extends Character implements Healer {
+     private final int healCapacity;
+
+    public Sorcerer(String name, int maxHealth, int healCapacity) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+
+    @Override
+    public void heal(Character obj) {
+        obj.setCurrentHealth(healCapacity);
+    }
+
+    @Override
+    public String toString() {
+        if (getCurrentHealth() == 0) {
+            return getName() + " is a dead sorcerer. So bad, it could heal " + this.healCapacity + " HP.";
+        }
+        return getName() + " is a sorcerer with " + getCurrentHealth() + " HP. It can heal " + getHealCapacity() + " HP.";
+    }
+}
