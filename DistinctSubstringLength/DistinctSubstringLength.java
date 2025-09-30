bb1d42e57b4081435e23fecf44d6239b09c83c42
ilyassatlassi <ilyassatlassi@gmail.com>
2025-09-30 19:11:30 +0100

    add anagaram

---
+++ DistinctSubstringLength/DistinctSubstringLength.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DistinctSubstringLength {
+    public int maxLength(String s) {
+        // Implementation to find the length of the longest substring without repeating
+        if (s.isEmpty()) {
+            return 0;
+        }
+        int count = 1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            String fir
+           if () {
+            
+           } 
+        }
+
+    }
+}
--- MultiplicationTable/MultiplicationTable.java
+++
@@ -1,8 +0,0 @@
-public class MultiplicationTable {
-    public static void generate(int num) {
-        // your code here
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(num + " " + "x " + i + " = " + num * i);
-        }
-    }
-}
