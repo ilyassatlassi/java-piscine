d48934728d78dc7238faa33e24885cefe1aec8f0
ilyassatlassi <ilyassatlassi@gmail.com>
2025-10-04 16:19:55 +0100

    StreamMap

---
+++ StreamMap/StreamMap.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamMap {
+    public static Integer sumOfStringLength(Stream<String> s) {
+        // your code here
+        return s.mapToInt(String::length).sum();
+    }
+
+    public static List<String> upperCaseAllString(Stream<String> s) {
+        // your code here
+        return s.map(String::toUpperCase).collect(Collectors.toList());
+    }
+
+    public static Set<Integer> uniqIntValuesGreaterThan42(Stream<Double> s) {
+        // your code here
+        return s.filter(val -> val > 42).map(Double::intValue).collect(Collectors.toSet());
+    }
+}
